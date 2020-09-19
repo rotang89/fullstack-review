4f88f5024ae64b0ef37822e8fddf06a01cf211a1
Johnny Tang <email.john.e@gmail.com>
2020-09-19 10:52:52 -0700

    filter top 25 and organized front end

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 const RepoList = (props) => {
       return (
-      <div>
-        <h4> Repo List Component </h4>
-        There are {props.repos.length} repos.
+        <div>
+        <h3> Repo List Component </h3>
+        <h4>There are {props.repos.length} repos.</h4>
+        <h4 className = 'details'>Repo</h4>
+        <h4 className = 'owner'>Owner</h4>
         {props.repos.map((repo) => {
           return (
-            <div>
-              <div>Name: {repo.Repo_Name}</div>
-              <div>Score: {repo.Score}</div>
+            <div className='container' key={repo.ID}>
+              <div className='details'>
+                <div>{repo.Repo_Name}</div>
+                <div><a href={repo.URL} target="_blank">{repo.URL}</a></div>
+                <div>{repo.Description}</div>
+              </div>
+              <div className='owner'>
+                <div>{repo.Owner.name}</div>
+                <div><a href={repo.Owner.url} target="_blank">{repo.Owner.url}</a></div>
+              </div>
             </div>
           )
         })}
